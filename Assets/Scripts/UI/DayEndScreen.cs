5c074b1daa78553bfc99cd7156472875a3f80597
absy_2201 <abhimanyu.chattopadhyay@gmail.com>
2022-05-31 13:14:16 +0100

    Added Day Number To Day End Screen

---
+++ Assets/Scripts/UI/DayEndScreen.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DayEndScreen : MonoBehaviour
+{
+    [SerializeField]
+    private GameplayTracking gameplayTracking;
+
+    [SerializeField]
+    private TextMeshProUGUI dayEndText;
+
+    private void OnEnable()
+    {
+        dayEndText.text = "End of Day " + gameplayTracking.completedDays;
+
+    }
+}

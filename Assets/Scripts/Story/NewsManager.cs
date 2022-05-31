271a2794f2459fb85ed85c7521cdf9a553a6a2ec
absy_2201 <abhimanyu.chattopadhyay@gmail.com>
2022-05-31 12:25:27 +0100

    Added few Story News Articles, Some Color Changes to UI, Added Functionality to Disable Fluff News

--- Assets/Scripts/Story/NewsManager.cs
+++ Assets/Scripts/Story/NewsManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class NewsManager : MonoBehaviour
 {
+    public bool generateFluffNews;
+
     [SerializeField]
     private GameObject newsPrefab;
     [SerializeField]
@@ -40,45 +42,48 @@ public class NewsManager : MonoBehaviour
         finalNewsFeed.Clear();
         todaysNewsData.Clear();
 
-        int numberOfArticles = Random.Range(minArticlesPerDay, maxArticlesPerDay);
+        if(generateFluffNews)
+        {
+            int numberOfArticles = Random.Range(minArticlesPerDay, maxArticlesPerDay);
 
-        if(storyManager.currentChapter == StoryManager.Chapter.One)
-        {
-            allNewsData = chapterOneFluffNews;
-        }
-        else if(storyManager.currentChapter == StoryManager.Chapter.Two)
-        {
-            allNewsData = chapterTwoFluffNews;
-        }
-        else if (storyManager.currentChapter == StoryManager.Chapter.Three)
-        {
-            allNewsData = chapterThreeFluffNews;
-        }
-
-        if (allNewsData.Count < numberOfArticles)
-        {
-            Debug.Log("NOT ENOUGH ARTICLES IN LIST!");
-            return;
-        }
-        else
-        {
-            for (int i = 0; i < numberOfArticles; i++)
+            if (storyManager.currentChapter == StoryManager.Chapter.One)
             {
-                int x = Random.Range(1, allNewsData.Count);
-                if (!todaysNewsData.Contains(allNewsData[x]))
-                {
-                    todaysNewsData.Add(allNewsData[x]);
-                    allNewsData.Remove(allNewsData[x]); //Uncomment to make it so news doesn't repeat on other days
-                }
-                else
-                {
-                    continue;
-                }
+                allNewsData = chapterOneFluffNews;
+            }
+            else if (storyManager.currentChapter == StoryManager.Chapter.Two)
+            {
+                allNewsData = chapterTwoFluffNews;
+            }
+            else if (storyManager.currentChapter == StoryManager.Chapter.Three)
+            {
+                allNewsData = chapterThreeFluffNews;
             }
 
-            foreach (var newsArticle in todaysNewsData)
+            if (allNewsData.Count < numberOfArticles)
             {
-                finalNewsFeed.Add(dateTimeSystem.PickRandomTime(), newsArticle);
+                Debug.Log("NOT ENOUGH ARTICLES IN LIST!");
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < numberOfArticles; i++)
+                {
+                    int x = Random.Range(1, allNewsData.Count);
+                    if (!todaysNewsData.Contains(allNewsData[x]))
+                    {
+                        todaysNewsData.Add(allNewsData[x]);
+                        allNewsData.Remove(allNewsData[x]); //Uncomment to make it so news doesn't repeat on other days
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                foreach (var newsArticle in todaysNewsData)
+                {
+                    finalNewsFeed.Add(dateTimeSystem.PickRandomTime(), newsArticle);
+                }
             }
         }
 

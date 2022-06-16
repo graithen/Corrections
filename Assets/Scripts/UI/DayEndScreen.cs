f9bd2675df92644f1cfe30c45b1389de843a1e66
absy_2201 <abhimanyu.chattopadhyay@gmail.com>
2022-06-16 13:27:58 +0100

    Added Fade-In for Day End Screen and Fixed Typo

--- Assets/Scripts/UI/DayEndScreen.cs
+++ Assets/Scripts/UI/DayEndScreen.cs
@@ -6,14 +6,46 @@ using TMPro;
 
 public class DayEndScreen : MonoBehaviour
 {
-    [SerializeField]
-    private GameplayTracking gameplayTracking;
+    [Header("UI")]
+    public Image dayEndScreen;
+    public TextMeshProUGUI dayEndText;
+    public Button dayEndButton;
 
-    [SerializeField]
-    private TextMeshProUGUI dayEndText;
-
-    private void OnEnable()
+    public void ActivateDayEndScreen()
     {
-        dayEndText.text = "A Few Days Later ...";
+        StartCoroutine(coActivateDayEndScreen());
+    }
+
+    public void DeactivateDayEndScreen()
+    {
+        StartCoroutine(coDeactivateDayEndScreen());
+    }
+
+    private IEnumerator coActivateDayEndScreen()
+    {
+        dayEndScreen.gameObject.SetActive(true);
+
+        while (dayEndScreen.color.a < 1)
+        {
+            dayEndScreen.color = new Color(dayEndScreen.color.r, dayEndScreen.color.g, dayEndScreen.color.b, dayEndScreen.color.a + Time.deltaTime);
+            yield return null;
+        }
+
+        dayEndText.gameObject.SetActive(true);
+        dayEndButton.gameObject.SetActive(true);
+    }
+
+    private IEnumerator coDeactivateDayEndScreen()
+    {
+        dayEndText.gameObject.SetActive(false);
+        dayEndButton.gameObject.SetActive(false);
+
+        while (dayEndScreen.color.a > 0)
+        {
+            dayEndScreen.color = new Color(dayEndScreen.color.r, dayEndScreen.color.g, dayEndScreen.color.b, dayEndScreen.color.a - Time.deltaTime);
+            yield return null;
+        }
+
+        dayEndScreen.gameObject.SetActive(false);
     }
 }
